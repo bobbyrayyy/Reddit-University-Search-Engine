0efe5751cd04ef28f25be80c3fa71255655575e9
Bob Lin <boblinbob0906@gmail.com>
2023-03-27 11:32:01 +0800

    changes to frontend cde and search engine logic

--- frontend.py
+++ frontend.py
@@ -1,25 +1,50 @@
 import PySimpleGUI as sg
+import json
 import search_engine
 
 sg.theme('Reddit')
+font = ('Courier New', 11)
+
+headings = ['Body', 'Post Title', 'Subreddit', 'URL']
+data = []   # Empty data
+col_widths = list(map(lambda x:len(x)+2, headings)) # find the widths of columns in character.
+max_col_width = len('ParameterNameToLongToFitIntoAColumn')+2  # Set max midth of all columns of data to show
 
 # Define the window layout
 layout = [
     [sg.Text('Search Engine', font=('Helvetica', 20))],
     [sg.Text('Enter your query:')],
-    [sg.InputText()],
+    [sg.InputText(size=(200, 1))],
     [sg.Button('Search')],
     [sg.Text('Results:', font=('Helvetica', 14))],
-    [sg.Listbox(values=[], size=(60, 15), font=('Helvetica', 12), key='results')]
+    # [sg.Listbox(values=[], size=(60, 15), font=('Helvetica', 12), key='results')],
+    [sg.Table(
+        values=data,                # Empty data must be with auto_size_columns=False
+        headings=headings,
+        auto_size_columns=False,    # For empty data
+        vertical_scroll_only=False, # Required if column widths changed and width of table not changed
+        hide_vertical_scroll=True,  # Not required if no more rows of data
+        def_col_width=20,
+        num_rows=10,
+        col_widths=col_widths,      # Define each column width as len(string)+2
+        font=font,                  # Use monospaced font for correct width
+        key='-TABLE-')]
 ]
 
 # Create the window
-window = sg.Window('Search Engine', layout, size=(800, 600))
+window = sg.Window('Reddit University Search Engine', layout, finalize=True, size=(1000, 800))
+
+char_width = sg.Text.char_width_in_pixels(font)     # Get character width in pixel
+table = window['-TABLE-']
+table_widget = table.Widget
+table.expand(expand_x=True, expand_y=True)          # Expand table in both directions of 'x' and 'y'
+for cid in headings:
+    table_widget.column(cid, stretch=True)          # Set column stretchable when window resize
 
 # Event loop
 while True:
     event, values = window.read()
-    print(event, values)
+    # print(event, values)
 
     # Close the window if the user clicks the X button or presses Esc
     if event == sg.WINDOW_CLOSED or event == 'Exit':
@@ -28,10 +53,26 @@ while True:
     # If the user clicks the Search button, send a request to the search engine backend
     if event == 'Search':
         query = values[0]
-        # TODO: Send a POST request to the search engine backend with the query and get the results
-        # results = ['Result 1', 'Result 2', 'Result 3']
         results = search_engine.search(query)
-        window['results'].update(values=results)
+
+        table_data = []
+        for row in results:
+            table_data.append([row['Comment_Body'], row['Post_Title'], row['Subreddit'], row['URL']])
+        # window['-TABLE-'].update(values=table_data, visible=True)
+        # window['table'].set_size((200, 50))
+
+        all_data = [headings] + table_data    
+        # Find width in pixel and 2 extra characters for each column
+        col_widths = [min([max(map(len, columns))+2, max_col_width])*char_width for columns in zip(*all_data)]
+        table.update(values=table_data)                   # update all new data
+        # Redraw table to update new size of table if horizontal scrollbar not used, care if widget too large to fit your window or screen.
+        table_widget.pack_forget()
+        for cid, width in zip(headings, col_widths):    # Set width for each column
+            table_widget.column(cid, width=width)
+        # table_widget.column('Subreddit', width=5)
+        table_widget.pack(side='left', fill='both', expand=True)    # Redraw table
+
+
 
 # Close the window
 window.close()
--- search_engine.py
+++ search_engine.py
@@ -1,38 +1,53 @@
 import requests
+import pandas as pd
 
 solr_url = "http://localhost:8983/solr/new_core/select"
 
-def search(query):
-    # Define the Solr API URL and query parameters
+schools = ['ntu', 'nus', 'smu', 'sit', 'sutd']
 
+def search(query):
+    # Search in both post title and comments
+    formatted_query = "(Post_Title:" + str(query) + " OR Comment_Body:" + str(query) +")"  # to search in both posts and comments
+
+    # Decide which subreddits to include in the search
+    subs_to_include = []
+    for school in schools:
+        if school in query:
+            subs_to_include.append(school)
+
+    formatted_query += " AND (Subreddit:'sgexams'"
+    if len(subs_to_include) > 0:
+        for i in range(0, len(subs_to_include)):
+            formatted_query += " OR Subreddit:" + subs_to_include[i]
+    formatted_query += ")"
+
+    # Define the Solr API URL and query parameters
     params = {
-        'q': "Post_Title:" + query + " OR Commment_Body:" + query,  # to search in both posts and comments
-        'rows': 100
-        # 'fl': 'title, content',
-        # 'wt': 'json'
+        'q': formatted_query,
+        'rows': 5,  # Top k=5 results
+        'boost': 'product(Total_Score, score)' # Boost query by multiplying score with Post Score field
+
     }
 
-    # Send a POST request to the Solr API with the query parameters
-    # response = requests.post(url, params=params)
-    response = requests.get(solr_url, params=params)
-    print(response)
+    try:
+        # Send a GET request to the Solr API with the query parameters
+        response = requests.get(solr_url, params=params)
+        # Check the response status code
+        if response.status_code == 200:
+            # The request was successful
+            # Parse the JSON response
+            response_json = response.json()
+            # Extract the search results from the response
+            results = response_json.get("response", {}).get("docs", [])
+        else:
+            # The request failed
+            print("Solr request failed with status code:", response.status_code)
+            results = ['Failed']
 
-    results = ['Failed']
-    # Check the response status code
-    if response.status_code == 200:
-        # The request was successful
-        # Parse the JSON response
-        response_json = response.json()
-        # Extract the search results from the response
-        results = response_json.get("response", {}).get("docs", [])
-        # Print the first search result
-        if len(results) > 0:
-            print(results[0])
-    else:
-        # The request failed
-        print("Solr request failed with status code:", response.status_code)
-
+    except requests.exceptions.RequestException as e:
+        # An error occurred while sending the request
+        print("Error sending Solr request:", e)
+        results = ['Failed']
+        
+    print(pd.DataFrame(results))
     return results
-
-
-
